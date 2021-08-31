255d6afbfdb4510629431723ee17b0572833e40e
krys64 <chris.luro@gmail.com>
2021-08-31 09:43:49 +0200

    Add files via upload

---
+++ Editor/ColorPicker.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class ColorPicker : EditorWindow
+{
+    [SerializeField]
+    Color color;
+    [SerializeField]
+    string hexColor;
+    string myString = "Hello World";
+    bool groupEnabled;
+    bool myBool = true;
+    float myFloat = 1.23f;
+
+    [MenuItem("Window/Color Picker")]
+    static void Init()
+    {
+        ColorPicker window = (ColorPicker)EditorWindow.GetWindow(typeof(ColorPicker));
+        window.Show();
+    }
+
+    void OnGUI()
+    {
+        color = EditorGUILayout.ColorField("color", color);
+        hexColor = EditorGUILayout.TextField("hexColor","#"+ ColorUtility.ToHtmlStringRGBA(color));
+
+        
+    }
+}
